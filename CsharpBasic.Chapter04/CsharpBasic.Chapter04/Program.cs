7a2194c2e53fe019127f4fd21241a5474b9b98ce
Lee Yunbi <dbsql10145@gmail.com>
2022-08-31 22:14:53 +0900

    Chater04 반복문 정리 내용 커밋

--- CsharpBasic.Chapter04/CsharpBasic.Chapter04/Program.cs
+++ CsharpBasic.Chapter04/CsharpBasic.Chapter04/Program.cs
@@ -16,8 +16,17 @@ namespace CsharpBasic.Chapter04
             //Examples.Ex002 ex002 = new Examples.Ex002();
             //ex002.Run();
 
-            Examples.Ex005 ex005 = new Examples.Ex005();
-            ex005.Run();
+            //Examples.Ex005 ex005 = new Examples.Ex005();
+            //ex005.Run();
+
+            //Examples.Ex006 ex006 = new Examples.Ex006();
+            //ex006.Run();
+
+            //Examples.Ex007 ex007 = new Examples.Ex007();
+            //ex007.Run();
+
+            Examples.Ex009 ex009 = new Examples.Ex009();
+            ex009.Run();
 
             //Works.Work01 work01 = new Works.Work01();
             //work01.Run();
